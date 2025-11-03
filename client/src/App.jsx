ea7c0932b818f8138f3b6a6585895cc4ade98764
Alex Kachur <alexkachur98@gmail.com>
2025-11-03 18:44:15 -0500

    refactor: collapse router into app entry

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,31 @@
  * @file App.jsx
  * @author Alex Kachur
  * @since 2025-10-31
- * @purpose The root component that wires the Dream Home Real Estate routes.
+ * @purpose Hosts the application's routing structure and shared layout.
  */
-import MainRouter from './MainRouter.jsx';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout.jsx';
+import Home from './pages/Home.jsx';
+import StaffMenu from './pages/StaffMenu.jsx';
+import BranchMenu from './pages/BranchMenu.jsx';
+import ClientMenu from './pages/ClientMenu.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
 
 export default function App() {
     return (
-        <MainRouter />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="staff" element={<StaffMenu />} />
+                    <Route path="branches" element={<BranchMenu />} />
+                    <Route path="clients" element={<ClientMenu />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="register" element={<Register />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 }
